3fd6195eddd0e651c29ae3982f9a094408a044e7
Victor Castro <152634561+vitinhozy@users.noreply.github.com>
2024-11-19 17:42:46 -0300

    v1

---
+++ javascript/aula 3 - while e dom/script/script.js
@@ -0,0 +1,19 @@
+//DOM
+// URL - LOCATION
+// "BOTAO DE VOLTAR" - HISTORY
+// "SITE / CONTEUDO" - DOCUMENT 
+//se innerText - apenas texto
+// se innerHTML - texto e código
+
+
+//window.location = ""
+
+//document.getElementById('catalogo').innerHTML = '<h3>OI</h3>'
+//document.querySelector('#catalogo').innerHTML = 'olá'
+
+function registrarNome(){
+    nome = document.querySelector('#inputNome').value
+    document.querySelector('#catalogo').innerHTML =`
+    
+    `    
+}
